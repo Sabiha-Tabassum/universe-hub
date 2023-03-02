2fb4badda86c6b6ae82d89216c1255619b7a7db2
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-03-03 00:59:23 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -9,9 +9,25 @@ const showData = Elements =>{
         Elements = Elements.slice(0, 6);
     Elements.forEach(element => {
         console.log(element);
-        
+        const cardDiv = document.createElement('card')
+        cardDiv.classList.add('col')
+        cardDiv.innerHTML = `
+        <div class="card h-100">
+        <img src="${element.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h3 class="card-title">Features</h3>
+          <p>1.${element.features[0]}</p>
+          <p>2.${element.features[1]}</p>
+          <p>3.${element.features[2]}</p>
+          <h3 class="card-title">${element.name}</h3>
+          
+        </div>
+        </div>
+        `;
+        cardContainer.appendChild(cardDiv);
         
     });
+    
 
 
 }
